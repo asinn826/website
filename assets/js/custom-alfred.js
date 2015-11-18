ef58fa5fede9fc333e6ae976bce2d1f69c17e8ad
asinn826 <alfredsin826@hotmail.com>
2015-11-18 00:06:00 -0800

    website refresh. right after adding the photo gallery

---
+++ assets/js/custom-alfred.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+
+    $('.blink').blink({delay:600}); // 600ms blink interval.
+
+    $('#lunchbuddies').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#eloryze').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#parkcity').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#prescriptapp').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#nextbus').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#casecomp').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#business').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#compsci').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#lifestuff').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#sap_dev').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#cpscta').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#clca').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#econtutor').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#cuscsm').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#arcvpr').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+
+    $('#cmpvpa').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s'
+    });
+});
--- js/custom.js
+++
@@ -1,6 +0,0 @@
- function onLoad() {
-     window.onscroll = function() {
-         var speed = 8.0;
-         document.body.style.backgroundPosition = (-window.pageXOffset / speed) + "px " + (-window.pageYOffset / speed) + "px";
-     }
- }
